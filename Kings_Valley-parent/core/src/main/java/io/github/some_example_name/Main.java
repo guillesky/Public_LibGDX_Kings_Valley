42fd1b9adff9558452bce77e52db8436e5963925
guill <guill@DESKTOP-QAJAGOB>
2024-09-25 11:32:42 -0300

    Read levels

--- Kings_Valley-parent/core/src/main/java/io/github/some_example_name/Main.java
+++ Kings_Valley-parent/core/src/main/java/io/github/some_example_name/Main.java
@@ -1,34 +1,112 @@
 package io.github.some_example_name;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+import modelo.LevelItem;
+import util.Constantes;
 
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
+ * platforms.
+ */
+public class Main extends ApplicationAdapter
+{
+	private TiledMap map;
+	private AssetManager manager;
+	private int tileWidth, tileHeight, mapWidthInTiles, mapHeightInTiles, mapWidthInPixels, mapHeightInPixels;
+	private OrthographicCamera camera;
+	private OrthogonalTiledMapRenderer renderer;
+	private LevelItem[] levelItems;
 
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
+	@Override
+	public void create()
+	{
+		String fileName = "maps/level_03.tmx";
+		manager = new AssetManager();
+		manager.setLoader(TiledMap.class, new TmxMapLoader());
+		manager.load(fileName, TiledMap.class);
+		manager.finishLoading();
 
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
+		map = manager.get(fileName, TiledMap.class);
+		MapProperties properties = map.getProperties();
+		tileWidth = properties.get("tilewidth", Integer.class);
+		tileHeight = properties.get("tileheight", Integer.class);
+		mapWidthInTiles = properties.get("width", Integer.class);
+		mapHeightInTiles = properties.get("height", Integer.class);
+		mapWidthInPixels = mapWidthInTiles * tileWidth;
+		mapHeightInPixels = mapHeightInTiles * tileHeight;
+		camera = new OrthographicCamera(mapHeightInPixels * 4 / 3, mapHeightInPixels);
+
+		camera.position.x = mapWidthInPixels * .5f;
+		camera.position.y = mapHeightInPixels * .5f;
+		renderer = new OrthogonalTiledMapRenderer(map);
+		MapLayer layerFront = map.getLayers().get(1);
+		
+		Iterator it=map.getTileSets().iterator();
+		while(it.hasNext())
+			System.out.println(it.next());
+		System.out.println(map.getTileSets().getTileSet(0).size());
+		
+	}
+
+	@Override
+	public void render()
+	{
+		Gdx.gl.glClearColor(.5f, .0f, .0f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+		renderer.setView(camera);
+		renderer.render();
+	}
+
+	@Override
+	public void dispose()
+	{
+		manager.dispose();
+		this.map.dispose();
+		this.renderer.dispose();
+	}
+
+	private void readLevelItem()
+	{
+		MapLayers mapLayers = map.getLayers();
+		MapLayer layerBack = mapLayers.get(0);
+		MapLayer layerFront = mapLayers.get(1);
+		MapLayer layerObject = mapLayers.get(2);
+		this.levelItems = new LevelItem[layerObject.getObjects().getCount()];
+		for (int i = 0; i < layerObject.getObjects().getCount(); i++)
+		{
+			MapObject objeto = layerObject.getObjects().get(i);
+
+			MapProperties mp = objeto.getProperties();
+			String stype = (String) mp.get("type");
+			float fx = (float) mp.get("x");
+			float fy = (float) mp.get("y");
+			String sp0 = (String) mp.get("p0");
+			String sp1 = (String) mp.get("p1");
+			int type = Constantes.stringToInteger.get(stype);
+			int x = (int) (fx / 10);
+			int y = (int) (fy / 10);
+			int p0 = Integer.parseInt(sp0);
+			int p1 = Integer.parseInt(sp1);
+			LevelItem levelItem = new LevelItem(type, x, y, p0, p1);
+			this.levelItems[i] = levelItem;
+			System.out.println(levelItem);
+			System.out.println("******************************");
+		}
+	}
 }
---
+++ Kings_Valley-parent/core/src/main/java/modelo/LevelItem.java
@@ -0,0 +1,51 @@
+package modelo;
+
+import util.Constantes;
+
+public class LevelItem
+{
+	private int type;
+	private int x;
+	private int y;
+	private int p0;
+	private int p1;
+	
+	
+	
+	
+	public LevelItem(int type, int x, int y, int p0, int p1)
+	{
+		super();
+		this.type = type;
+		this.x = x;
+		this.y = y;
+		this.p0 = p0;
+		this.p1 = p1;
+	}
+	public int getType()
+	{
+		return type;
+	}
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
+	public int getP0()
+	{
+		return p0;
+	}
+	public int getP1()
+	{
+		return p1;
+	}
+	@Override
+	public String toString()
+	{
+		return "LevelItem [type=" + Constantes.identificacion.get(type) + ", x=" + x + ", y=" + y + ", p0=" + p0 + ", p1=" + p1 + "]";
+	}
+
+}
