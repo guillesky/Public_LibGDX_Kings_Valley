08bc9819b396b422acb75bd3ee8db57368fa3a79
Guille <Guille@DESKTOP-R4S3DVL>
2025-05-03 23:36:46 -0300

    converter

---
+++ Kings_Valley_Converter/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tiles" tilewidth="10" tileheight="10" tilecount="260" columns="20">
+ <image source="pics/tiles2.png" width="200" height="130"/>
+</tileset>
---
+++ Kings_Valley_Converter/tiles_new.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tiles" tilewidth="10" tileheight="10" tilecount="144" columns="18">
+ <image source="pics/tiles4.png" width="180" height="80"/>
+</tileset>
