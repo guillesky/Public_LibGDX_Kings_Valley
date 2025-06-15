30d43ed8ccacd234ee4f120e29fd371f70a41c2d
Guille <Guille@DESKTOP-R4S3DVL>
2025-06-15 01:36:37 -0300

    Momia zombie. Ver salto de momias

--- Kings_Valley-parent/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Lwjgl3Launcher.java
+++ Kings_Valley-parent/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Lwjgl3Launcher.java
@@ -13,8 +13,8 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-    	 return new Lwjgl3Application(new Main(), getDefaultConfiguration());
-    //   return new Lwjgl3Application(new PNGMerger(), getDefaultConfiguration());
+   	 return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+   //   return new Lwjgl3Application(new PNGMerger(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
