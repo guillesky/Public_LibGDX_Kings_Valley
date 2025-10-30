dc4d96a79b1d452911e7ca09c8d0a70ef8d35681
Guillermo Lazzurri <guillesky@gmail.com>
2025-10-30 08:11:21 -0300

    Funcionan los menues

--- Kings_Valley-parent/core/src/main/java/vista2D/ui/Controler2D.java
+++ Kings_Valley-parent/core/src/main/java/vista2D/ui/Controler2D.java
@@ -22,338 +22,340 @@ import facade.Facade;
  */
 public class Controler2D extends AbstractControler implements KVEventListener
 {
-    private ClickListener clickListener;
-    private ChangeListener changeListener;
-    private UI2D ui;
+	private ClickListener clickListener;
+	private ChangeListener changeListener;
+	private UI2D ui;
 
-    /**
-     * Contructor de clase
-     * 
-     * @param view Objeto que representa la vista del patron.
-     */
-    public Controler2D(IView view)
-    {
-	super(view);
-	this.ui = (UI2D) view;
-	this.clickListener = new ClickListener()
+	/**
+	 * Contructor de clase
+	 * 
+	 * @param view Objeto que representa la vista del patron.
+	 */
+	public Controler2D(IView view)
 	{
-	    @Override
-	    public void clicked(InputEvent event, float x, float y)
-	    {
-		Actor actor = event.getTarget();
-
-		while (actor != null && actor.getUserObject() == null)
+		super(view);
+		this.ui = (UI2D) view;
+		this.clickListener = new ClickListener()
 		{
-		    actor = actor.getParent();
-		}
-		int action = (int) actor.getUserObject();
-		switch (action)
+			@Override
+			public void clicked(InputEvent event, float x, float y)
+			{
+				Actor actor = event.getTarget();
+
+				while (actor != null && actor.getUserObject() == null)
+				{
+					actor = actor.getParent();
+				}
+				int action = (int) actor.getUserObject();
+				switch (action)
+				{
+				case AbstractControler.NEW_GAME:
+				{
+					doNewGame();
+					break;
+
+				}
+				case AbstractControler.EXIT:
+				{
+					doExit();
+					break;
+
+				}
+
+				case AbstractControler.RETRY:
+					doRetry();
+
+					break;
+
+				case AbstractControler.MAIN_MENU:
+					Controler2D.this.doBackToMainMenu();
+					break;
+				case AbstractControler.SHOW_MAP:
+					doShowMap();
+					break;
+				case AbstractControler.HIDE_MAP:
+					doHideMap();
+					break;
+				}
+			}
+
+		};
+
+		this.changeListener = new ChangeListener()
 		{
-		case AbstractControler.NEW_GAME:
-		{
-		    doNewGame();
-		    break;
+			@Override
+			public void changed(ChangeEvent event, Actor actor)
+			{
+				while (actor != null && actor.getUserObject() == null)
+				{
+					actor = actor.getParent();
+				}
+				int action = (int) actor.getUserObject();
+				Slider sl = (Slider) actor;
+				switch (action)
+				{
+				case AbstractControler.MASTER_VOLUME:
+					changeMasterVolume(sl.getValue());
+					break;
+				case AbstractControler.MUSIC_VOLUME:
+					changeMusicVolume(sl.getValue());
+					break;
+				case AbstractControler.FX_VOLUME:
+					changeSoundsVolume(sl.getValue());
+					break;
 
-		}
-		case AbstractControler.EXIT:
-		{
-		    doExit();
-		    break;
+				}
+			}
 
-		}
-
-		case AbstractControler.RETRY:
-		    doRetry();
-
-		    break;
-
-		case AbstractControler.MAIN_MENU:
-		    Controler2D.this.doBackToMainMenu();
-		    break;
-		case AbstractControler.SHOW_MAP:
-		    doShowMap();
-		    break;
-		case AbstractControler.HIDE_MAP:
-		    doHideMap();
-		    break;
-		}
-	    }
-
-	};
-
-	this.changeListener = new ChangeListener()
-	{
-	    @Override
-	    public void changed(ChangeEvent event, Actor actor)
-	    {
-		while (actor != null && actor.getUserObject() == null)
-		{
-		    actor = actor.getParent();
-		}
-		int action = (int) actor.getUserObject();
-		Slider sl = (Slider) actor;
-		switch (action)
-		{
-		case AbstractControler.MASTER_VOLUME:
-		    changeMasterVolume(sl.getValue());
-		    break;
-		case AbstractControler.MUSIC_VOLUME:
-		    changeMusicVolume(sl.getValue());
-		    break;
-		case AbstractControler.FX_VOLUME:
-		    changeSoundsVolume(sl.getValue());
-		    break;
-
-		}
-	    }
-
-	};
-
-    }
-
-    /**
-     * Llamado para cambiar el volumen de los efectos de sonido
-     * 
-     * @param value Valor del volumen. Debe estar comprendido entre 0 y 100
-     */
-    protected void changeSoundsVolume(float value)
-    {
-	Facade.getInstance().setSoundsVolume(value / 100f);
-
-    }
-
-    /**
-     * Llamado para cambiar el volumen de la musica
-     * 
-     * @param value Valor del volumen. Debe estar comprendido entre 0 y 100
-     */
-    protected void changeMusicVolume(float value)
-    {
-	Facade.getInstance().setMusicVolume(value / 100f);
-
-    }
-
-    /**
-     * Llamado para cambiar el volumen general
-     * 
-     * @param value Valor del volumen. Debe estar comprendido entre 0 y 100
-     */
-    protected void changeMasterVolume(float value)
-    {
-	Facade.getInstance().setMasterVolume(value / 100f);
-
-    }
-
-    /**
-     * Retorna el objeto que responde a los clicks de los botones
-     * 
-     * @return El objeto que responde a los clicks de los botones
-     */
-    public ClickListener getInputListener()
-    {
-	return clickListener;
-    }
-
-    /**
-     * Llamado al pedir salir del juego
-     */
-    protected void doExit()
-    {
-
-	Gdx.app.exit();
-    }
-
-    /**
-     * Retorna un objeto changeListener que responde a los cambios de volumenes
-     * 
-     * @return Objeto changeListener que responde a los cambios de volumenes
-     */
-    public ChangeListener getChangeListener()
-    {
-	return changeListener;
-    }
-
-    /**
-     * Llamado al iniciar un nuevo juego
-     */
-    private void doNewGame()
-    {
-	Facade.getInstance().startNewGame(this.view.getDificultLevel());
-    }
-
-    /**
-     * Llamado al cambiar el idioma
-     * 
-     * @param languageName nombre del nuevo idioma
-     */
-    public void changeLanguage(String languageName)
-    {
-	Facade.getInstance().changeLanguage(languageName);
-	this.view.updateLanguage();
-    }
-
-    @Override
-    public void eventFired(int eventCode, Object param)
-    {
-	switch (eventCode)
-	{
-	case KVEventListener.PAUSED_IS_CHANGED:
-	{
-	    boolean isPaused = (boolean) param;
-	    if (isPaused)
-	    {
-		this.ui.doUiInGame();
-
-	    } else
-	    {
-		this.ui.doEnterGame();
-		doHideMap();
-
-	    }
-	    break;
-	}
-
-	case KVEventListener.ENTERING_LEVEL:
-	{
-	    this.ui.doEnteringLevel();
-	    break;
-	}
-
-	case KVEventListener.GAME_OVER:
-	{
-	    doMainMenu();
-	    break;
-	}
-
-	case KVEventListener.FINISH_ALL_LEVELS:
-	{
-	    doFinishAllLevels();
-	    break;
-	}
+		};
 
 	}
 
-    }
+	/**
+	 * Llamado para cambiar el volumen de los efectos de sonido
+	 * 
+	 * @param value Valor del volumen. Debe estar comprendido entre 0 y 100
+	 */
+	protected void changeSoundsVolume(float value)
+	{
+		Facade.getInstance().setSoundsVolume(value / 100f);
 
-    /**
-     * Llamado para cuando se finalizaron todos los niveles del juego. Se debe
-     * habilitar la opcion de cambio de dificultad
-     */
-    private void doFinishAllLevels()
-    {
-	Facade.getInstance().finishAllLevels();
-    }
+	}
 
-    @Override
-    public void updateframe(float deltaTime)
-    {
+	/**
+	 * Llamado para cambiar el volumen de la musica
+	 * 
+	 * @param value Valor del volumen. Debe estar comprendido entre 0 y 100
+	 */
+	protected void changeMusicVolume(float value)
+	{
+		Facade.getInstance().setMusicVolume(value / 100f);
 
-    }
+	}
 
-    /**
-     * Llamado al ocultar el mapa
-     */
-    private void doHideMap()
-    {
-	Facade.getInstance().hideMap();
-    }
+	/**
+	 * Llamado para cambiar el volumen general
+	 * 
+	 * @param value Valor del volumen. Debe estar comprendido entre 0 y 100
+	 */
+	protected void changeMasterVolume(float value)
+	{
+		Facade.getInstance().setMasterVolume(value / 100f);
 
-    /**
-     * Llamado al mostrar el mapa
-     */
-    private void doShowMap()
-    {
-	Facade.getInstance().showMap();
-    }
+	}
 
-    /**
-     * Llamado al ir o regresar al menu principal desde la UI inicial
-     */
-    private void doMainMenu()
-    {
-	Facade.getInstance().mainMenu();
+	/**
+	 * Retorna el objeto que responde a los clicks de los botones
+	 * 
+	 * @return El objeto que responde a los clicks de los botones
+	 */
+	public ClickListener getInputListener()
+	{
+		return clickListener;
+	}
 
-    }
+	/**
+	 * Llamado al pedir salir del juego
+	 */
+	protected void doExit()
+	{
 
-    /**
-     * Llamado al volver a la pantalla inicial durante el juego
-     */
-    protected void doBackToMainMenu()
-    {
-	Game.getInstance().pressPause();
-	Game.getInstance().endGame();
-    }
+		Gdx.app.exit();
+	}
 
-    /**
-     * Llamado para reintentar el juego (perdiendo una vida)
-     */
-    protected void doRetry()
-    {
-	Facade.getInstance().retry();
-    }
+	/**
+	 * Retorna un objeto changeListener que responde a los cambios de volumenes
+	 * 
+	 * @return Objeto changeListener que responde a los cambios de volumenes
+	 */
+	public ChangeListener getChangeListener()
+	{
+		return changeListener;
+	}
 
-    /**
-     * Retorna los creditos del juego en el idioma correspondiente
-     * 
-     * @return Los creditos del juego en el idioma correspondiente
-     */
-    protected String getCredits()
-    {
-	return Facade.getInstance().getCredits();
-    }
+	/**
+	 * Llamado al iniciar un nuevo juego
+	 */
+	private void doNewGame()
+	{
+		//System.out.println("Extendido: " + this.view.isExtendedVersion() + " Episodio: " + this.view.getEpisode());
+		
+		Facade.getInstance().startNewGame(this.view.getDificultLevel());
+	}
 
-    /**
-     * Retorna true si el juego fue terminado al menos una vez, false en caso
-     * contrario
-     * 
-     * @return true si el juego fue terminado al menos una vez, false en caso
-     *         contrario
-     */
-    public boolean isFinishedOneTime()
-    {
+	/**
+	 * Llamado al cambiar el idioma
+	 * 
+	 * @param languageName nombre del nuevo idioma
+	 */
+	public void changeLanguage(String languageName)
+	{
+		Facade.getInstance().changeLanguage(languageName);
+		this.view.updateLanguage();
+	}
 
-	return Facade.getInstance().getGameConfig().isFinishedOneTime();
-    }
+	@Override
+	public void eventFired(int eventCode, Object param)
+	{
+		switch (eventCode)
+		{
+		case KVEventListener.PAUSED_IS_CHANGED:
+		{
+			boolean isPaused = (boolean) param;
+			if (isPaused)
+			{
+				this.ui.doUiInGame();
 
-    /**
-     * Retorna el volumen general
-     * 
-     * @return volumen general
-     */
-    protected float getMasterVolume()
-    {
-	return Facade.getInstance().getGameConfig().getMasterVolume();
-    }
+			} else
+			{
+				this.ui.doEnterGame();
+				doHideMap();
 
-    /**
-     * Retorna el volumen de la musica
-     * 
-     * @return volumen de la musica
-     */
-    protected float getMusicVolume()
-    {
-	return Facade.getInstance().getGameConfig().getMusicVolume();
-    }
+			}
+			break;
+		}
 
-    /**
-     * Retorna el volumen de los efectos de sonido
-     * 
-     * @return volumen de los efectos de sonido
-     */
-    protected float getSoundsVolume()
-    {
-	return Facade.getInstance().getGameConfig().getSoundsVolume();
-    }
+		case KVEventListener.ENTERING_LEVEL:
+		{
+			this.ui.doEnteringLevel();
+			break;
+		}
 
-    /**
-     * Retorna los nombres de los idiomas posibles
-     * 
-     * @return Los nombres de los idiomas posibles
-     */
-    public Iterator<String> getLanguagesName()
-    {
+		case KVEventListener.GAME_OVER:
+		{
+			doMainMenu();
+			break;
+		}
 
-	return Facade.getInstance().getAllLanguages().getLanguagesName();
-    }
+		case KVEventListener.FINISH_ALL_LEVELS:
+		{
+			doFinishAllLevels();
+			break;
+		}
+
+		}
+
+	}
+
+	/**
+	 * Llamado para cuando se finalizaron todos los niveles del juego. Se debe
+	 * habilitar la opcion de cambio de dificultad
+	 */
+	private void doFinishAllLevels()
+	{
+		Facade.getInstance().finishAllLevels();
+	}
+
+	@Override
+	public void updateframe(float deltaTime)
+	{
+
+	}
+
+	/**
+	 * Llamado al ocultar el mapa
+	 */
+	private void doHideMap()
+	{
+		Facade.getInstance().hideMap();
+	}
+
+	/**
+	 * Llamado al mostrar el mapa
+	 */
+	private void doShowMap()
+	{
+		Facade.getInstance().showMap();
+	}
+
+	/**
+	 * Llamado al ir o regresar al menu principal desde la UI inicial
+	 */
+	private void doMainMenu()
+	{
+		Facade.getInstance().mainMenu();
+
+	}
+
+	/**
+	 * Llamado al volver a la pantalla inicial durante el juego
+	 */
+	protected void doBackToMainMenu()
+	{
+		Game.getInstance().pressPause();
+		Game.getInstance().endGame();
+	}
+
+	/**
+	 * Llamado para reintentar el juego (perdiendo una vida)
+	 */
+	protected void doRetry()
+	{
+		Facade.getInstance().retry();
+	}
+
+	/**
+	 * Retorna los creditos del juego en el idioma correspondiente
+	 * 
+	 * @return Los creditos del juego en el idioma correspondiente
+	 */
+	protected String getCredits()
+	{
+		return Facade.getInstance().getCredits();
+	}
+
+	/**
+	 * Retorna true si el juego fue terminado al menos una vez, false en caso
+	 * contrario
+	 * 
+	 * @return true si el juego fue terminado al menos una vez, false en caso
+	 *         contrario
+	 */
+	public boolean isFinishedOneTime()
+	{
+
+		return Facade.getInstance().getGameConfig().isFinishedOneTime();
+	}
+
+	/**
+	 * Retorna el volumen general
+	 * 
+	 * @return volumen general
+	 */
+	protected float getMasterVolume()
+	{
+		return Facade.getInstance().getGameConfig().getMasterVolume();
+	}
+
+	/**
+	 * Retorna el volumen de la musica
+	 * 
+	 * @return volumen de la musica
+	 */
+	protected float getMusicVolume()
+	{
+		return Facade.getInstance().getGameConfig().getMusicVolume();
+	}
+
+	/**
+	 * Retorna el volumen de los efectos de sonido
+	 * 
+	 * @return volumen de los efectos de sonido
+	 */
+	protected float getSoundsVolume()
+	{
+		return Facade.getInstance().getGameConfig().getSoundsVolume();
+	}
+
+	/**
+	 * Retorna los nombres de los idiomas posibles
+	 * 
+	 * @return Los nombres de los idiomas posibles
+	 */
+	public Iterator<String> getLanguagesName()
+	{
+
+		return Facade.getInstance().getAllLanguages().getLanguagesName();
+	}
 
 }
